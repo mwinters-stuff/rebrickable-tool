5eded421d93dc7e7c87df1e94809386c9845bcfe
Mathew Winters <mathew_winters@trimble.com>
2016-05-02 14:37:59 +1200

    Travis?

--- travis-runner.sh
+++ travis-runner.sh
@@ -3,24 +3,24 @@ set -o pipefail
 
 if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" = "5.1" ]
 then
-  git config --global user.email "samccone@gmail.com"
+  git config --global user.email "wintersandroid@winters.org.nz"
   git config --global user.name "auto deployer"
 
   # Stamp index.html with the date and time of PSK's deploying
-  date_value=`date`
-  sed -i.tmp1 "s/This is another card./This is another card. PSK Deployed on: $date_value/" app/index.html
+#  date_value=`date`
+#  sed -i.tmp1 "s/This is another card./This is another card. PSK Deployed on: $date_value/" app/index.html
 
   deploy_ghpages () {
     # Deploying to GitHub Pages! (http://polymerelements.github.io/polymer-starter-kit)
     echo Deploying to GitHub Pages
-    sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js
-    sed -i.tmp2 "s/<\/head>/\  \<script>'https:'!==window.location.protocol\&\&(window.location.protocol='https')<\/script>&/g" app/index.html
+#    sed -i.tmp "s/\/\/ app.baseUrl = '\/polymer-starter-kit/app.baseUrl = '\/polymer-starter-kit/" app/scripts/app.js
+#    sed -i.tmp2 "s/<\/head>/\  \<script>'https:'!==window.location.protocol\&\&(window.location.protocol='https')<\/script>&/g" app/index.html
     gulp build-deploy-gh-pages
     # Undoing Changes to PSK for GitHub Pages
-    cp app/scripts/app.js.tmp app/scripts/app.js
-    rm app/scripts/app.js.tmp
-    cp app/index.html.tmp2 app/index.html
-    rm app/index.html.tmp2
+#    cp app/scripts/app.js.tmp app/scripts/app.js
+#    rm app/scripts/app.js.tmp
+#    cp app/index.html.tmp2 app/index.html
+#    rm app/index.html.tmp2
   }
 
   deploy_firebase () {
@@ -43,11 +43,11 @@ then
   }
 
   deploy_ghpages
-  deploy_firebase
+#  deploy_firebase
 
   # Revert to orginal index.html and delete temp file
-  cp app/index.html.tmp1 app/index.html
-  rm app/index.html.tmp1
+#  cp app/index.html.tmp1 app/index.html
+#  rm app/index.html.tmp1
 elif [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]  && [ "$TRAVIS_NODE_VERSION" != "5.1" ]
 then
   echo "Do Nothing, only deploy with Node 5.1"
